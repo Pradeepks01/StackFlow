2c5b36c8ef5ee1708c104f98399ed831c64f8c93
Pradeepks01 <pradeep@stackflow.io>
2026-04-18 13:06:16 +0530

    feat(observability): add OpenTelemetry distributed tracing setup

---
+++ app/backend/observability/tracer.js
@@ -0,0 +1,27 @@
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+
+const sdk = new NodeSDK({
+    resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: 'stackflow-backend',
+    }),
+    instrumentations: [getNodeAutoInstrumentations()],
+});
+
+// For demonstration, we just export a start function
+const startTracer = () => {
+    sdk.start()
+        .then(() => console.log('Tracing initialized'))
+        .catch((error) => console.log('Error initializing tracing', error));
+};
+
+process.on('SIGTERM', () => {
+    sdk.shutdown()
+        .then(() => console.log('Tracing terminated'))
+        .catch((error) => console.log('Error terminating tracing', error))
+        .finally(() => process.exit(0));
+});
+
+module.exports = startTracer;
